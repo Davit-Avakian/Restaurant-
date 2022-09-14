581ccf37ee339e4d2830c92dd3edd62e54a4b7be
Davit Avakian <davitavakian05@gmail.com>
2022-09-14 19:30:20 +0400

    added draw menu from data object, scrollable menu with mouse drag and responsive for min 1000px

--- index.js
+++ index.js
@@ -1,14 +1,86 @@
+// DATA
+const menuData = {
+    pita: [
+        {
+            name: "Falafel", img: "./assets/images/PitaImage.webp", description: `Kale, falafel, poivron, tomate, betterave, carotte
+    ciboulette, houmous, fromage végétal, sauce à lail.`, price: "10.00$"
+        },
+        {
+            name: "Falafel", img: "./assets/images/PitaImage.webp", description: `Kale, falafel, poivron, tomate, betterave, carotte
+ciboulette, houmous, fromage végétal, sauce à lail.`, price: "10.00$"
+        },
+        {
+            name: "Falafel", img: "./assets/images/PitaImage.webp", description: `Kale, falafel, poivron, tomate, betterave, carotte
+ciboulette, houmous, fromage végétal, sauce à lail.`, price: "10.00$"
+        },
+        {
+            name: "Falafel", img: "./assets/images/PitaImage.webp", description: `Kale, falafel, poivron, tomate, betterave, carotte
+ciboulette, houmous, fromage végétal, sauce à lail.`, price: "10.00$"
+        }
+    ],
+
+    salad: [
+        {
+            name: "Kale cesar", img: "./assets/images/SaladImage.webp", description: `Kale, falafel, poivron, tomate, betterave, carotte
+    ciboulette, houmous, fromage végétal, sauce à lail.`, price: "10.00$"
+        },
+        {
+            name: "Kale cesar", img: "./assets/images/SaladImage.webp", description: `Kale, falafel, poivron, tomate, betterave, carotte
+ciboulette, houmous, fromage végétal, sauce à lail.`, price: "10.00$"
+        },
+        {
+            name: "Kale cesar", img: "./assets/images/SaladImage.webp", description: `Kale, falafel, poivron, tomate, betterave, carotte
+ciboulette, houmous, fromage végétal, sauce à lail.`, price: "10.00$"
+        },
+        {
+            name: "Kale cesar", img: "./assets/images/SaladImage.webp", description: `Kale, falafel, poivron, tomate, betterave, carotte
+ciboulette, houmous, fromage végétal, sauce à lail.`, price: "10.00$"
+        }
+    ],
+
+    lunch: [
+        {
+            name: "Salade dej", img: "./assets/images/LaunchImage.webp", description: `Kale, falafel, poivron, tomate, betterave, carotte
+    ciboulette, houmous, fromage végétal, sauce à lail.`, price: "10.00$"
+        },
+        {
+            name: "Salade dej", img: "./assets/images/LaunchImage.webp", description: `Kale, falafel, poivron, tomate, betterave, carotte
+ciboulette, houmous, fromage végétal, sauce à lail.`, price: "10.00$"
+        },
+        {
+            name: "Salade dej", img: "./assets/images/LaunchImage.webp", description: `Kale, falafel, poivron, tomate, betterave, carotte
+ciboulette, houmous, fromage végétal, sauce à lail.`, price: "10.00$"
+        },
+        {
+            name: "Salade dej", img: "./assets/images/LaunchImage.webp", description: `Kale, falafel, poivron, tomate, betterave, carotte
+ciboulette, houmous, fromage végétal, sauce à lail.`, price: "10.00$"
+        }
+    ],
+
+    snack: [
+        {
+            name: "Boule denergie", img: "./assets/images/SnackImage.webp", description: `Kale, falafel, poivron, tomate, betterave, carotte
+    ciboulette, houmous, fromage végétal, sauce à lail.`, price: "10.00$"
+        },
+        {
+            name: "Boule denergie", img: "./assets/images/SnackImage.webp", description: `Kale, falafel, poivron, tomate, betterave, carotte
+ciboulette, houmous, fromage végétal, sauce à lail.`, price: "10.00$"
+        },
+        {
+            name: "Boule denergie", img: "./assets/images/SnackImage.webp", description: `Kale, falafel, poivron, tomate, betterave, carotte
+ciboulette, houmous, fromage végétal, sauce à lail.`, price: "10.00$"
+        },
+        {
+            name: "Boule denergie", img: "./assets/images/SnackImage.webp", description: `Kale, falafel, poivron, tomate, betterave, carotte
+ciboulette, houmous, fromage végétal, sauce à lail.`, price: "10.00$"
+        },
+    ]
+};
+
 const MainDishesContainer = document.querySelector('.Main-Dishes-Container');
-
-const PitasContainer = document.querySelector('.Pitas-Container');
-const SaladsContainer = document.querySelector('.Salads-Container');
-const LaunchContainer = document.querySelector('.Launch-Container');
-const SnacksContainer = document.querySelector('.Snacks-Container');
-
 const categoryButtons = document.querySelectorAll('.Categories-Button');
 
 let prevButton;
-let prevContainer;
 
 categoryButtons.forEach(btn => {
     btn.addEventListener('click', categoryButtonEvent);
@@ -17,36 +89,87 @@ categoryButtons.forEach(btn => {
 function categoryButtonEvent(e) {
     const containerType = e.target.getAttribute('data-value');
 
-    if(prevButton) {
+    const dishesContainer = document.createElement('div');
+    dishesContainer.classList.add('Dish-Container');
+
+    if (prevButton) {
         prevButton.style.color = '#2a5631';
         prevButton.style.background = '#ffff';
-    }
-
-    if(prevContainer) {
-        prevContainer.style.display = 'none';
     }
 
     e.target.style.color = '#ffff';
     e.target.style.background = '#2a5631';
 
-    switch(containerType) {
-        case 'pita':
-            PitasContainer.style.display = 'flex';
-            prevContainer = PitasContainer;
-            break;
-        case 'salad':
-            SaladsContainer.style.display = 'flex';
-            prevContainer = SaladsContainer;
-            break;
-        case 'launch': 
-            LaunchContainer.style.display = 'flex';
-            prevContainer = LaunchContainer;
-            break;
-        case 'snack':
-            SnacksContainer.style.display = 'flex';
-            prevContainer = SnacksContainer;
-            break;
-    }
+    menuData[containerType].forEach(({ name, img, description, price }) => {
+        const dishItem = `
+                <div class="Dish-Item">
+                    <span class="Dish-Price">${price}</span>
+        
+                    <img src="${img}" class="Dish-Image" />
+        
+                    <div class="Dish-Desc">
+                        <span class="Dish-Desc-Title">${name}</span>
+                        <span class="Dish-Desc-Body">${description}</span>
+                        <button class="Dish-Desc-Button">COMMANDER EN LIGNE</button>
+                    </div>
+            </div>
+            `;
+
+        dishesContainer.innerHTML += dishItem;
+    })
+
+    dishesContainer.innerHTML += `
+        <div class="View-All-Container">
+            <h3>Pitas</h3>
+            <button>CONSULTER TOUS LES PLATS</button>
+        </div>
+    `;
+
+    MainDishesContainer.innerHTML = '';
+    MainDishesContainer.append(dishesContainer);
 
     prevButton = e.target;
+
+    dishesContainer.addEventListener('mousemove', (e) => mouseMoveEvent(e, dishesContainer));
+    dishesContainer.addEventListener('mousedown', (e) => startDragging(e, dishesContainer));
+    dishesContainer.addEventListener('mouseup', stopDragging);
+    dishesContainer.addEventListener('mouseleave', stopDragging);
 }
+
+
+// Dishes Containers dragging with mouse
+let mouseDown = false;
+let startX, scrollLeft;
+
+function startDragging(e, container) {
+    mouseDown = true;
+    startX = e.pageX - container.offsetLeft;
+    scrollLeft = container.scrollLeft;
+};
+function stopDragging() {
+    mouseDown = false;
+};
+
+function mouseMoveEvent(e, container) {
+    e.preventDefault();
+    if (!mouseDown) return;
+    const x = e.pageX - container.offsetLeft;
+    const scroll = x - startX;
+    container.scrollLeft = scrollLeft - scroll;
+}
+
+//Scroll Dishes Div On Left and Right Arrows Click
+const leftArrow = document.querySelector('.Left-Arrow');
+const rightArrow = document.querySelector('.Right-Arrow');
+
+leftArrow.addEventListener('click', () => {
+    const activeContainer = document.querySelector('.Main-Dishes-Container > div');
+
+    if (activeContainer) activeContainer.scrollLeft -= 380;
+})
+
+rightArrow.addEventListener('click', () => {
+    const activeContainer = document.querySelector('.Main-Dishes-Container > div');
+
+    if (activeContainer) activeContainer.scrollLeft += 380;
+})
